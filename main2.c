f5465a9c10abea4522ff1b9c25b9fd3b3a0ac134
david03-ops <david.josepha03@gmail.com>
2026-04-05 16:24:21 -0400

    definitions and struct for main2

--- main2.c
+++ main2.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define PAGE_SIZE 256
+#define PAGE_TABLE_SIZE 256
+#define TLB_SIZE 16
+#define DEFAULT_FRAME_COUNT 128
+#define MAX_FRAME_COUNT 1024
+#define INVALID -1
+
+typedef struct {
+    int page;
+    int frame;
+} TLBEntry;
+
